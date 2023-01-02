beed8897de03ea255708ef8325980c634941baff
jamesnascimento1994 <james.nascimento94@yahoo.com>
2023-01-01 23:37:03 -0500

    feat: finished functionality

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let btn = document.querySelector('.output-btn');
+let phoneNumberInput = document.querySelector('.phone-input');
+let output = document.querySelector('.output');
+let regex = /^[\d, \s, \+, \-]{5,20}/;
+
+
 
 btn.addEventListener('click',  () => {
-    let phoneNumberInput = document.getElementById('phone-input');
-    let output = document.querySelector('.output');
-    let regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
 
-    output.innerHTML = regex.test(phoneNumberInput) ? true : false;
+    if (phoneNumberInput.value == "") {
+        output.innerText = "Please Enter a Number";
+    } else if (phoneNumberInput.value.match(regex)) {
+        output.innerText = "This phone number is valid";
+    } else {
+        output.innerText = "This phone number is invalid";
+    }
 });
